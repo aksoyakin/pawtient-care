986579f1804ecaf6f67b700fc09d45dd4bed33d3
AKIN AKSOY <akinaksoyiletisim@gmail.com>
2024-10-06 22:29:16 +0300

    Initial commit

---
+++ pawtient-care-be/src/main/java/com/aksoyakin/pawtientcarebe/PawtientCareBeApplication.java
@@ -0,0 +1,13 @@
+package com.aksoyakin.pawtientcarebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawtientCareBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawtientCareBeApplication.class, args);
+    }
+
+}
