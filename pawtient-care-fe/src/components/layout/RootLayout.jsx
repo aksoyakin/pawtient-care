59858d1a470d72936de3811e30d3558ce11a4187
AKIN AKSOY <akinaksoyiletisim@gmail.com>
2024-10-20 19:57:58 +0300

    navbar

--- pawtient-care-fe/src/components/layout/RootLayout.jsx
+++ pawtient-care-fe/src/components/layout/RootLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom';
 import BackgroundImageSlider from "../common/BackgroundImageSlider.jsx";
+import NavBar from "./NavBar.jsx";
 
 const RootLayout = () => {
     return (
         <main>
+            <NavBar/>
             <BackgroundImageSlider/>
 
             <div>
